493c53eaef5271087cdbf5bb39353f6089658095
bendalgaard <bendalgaard@gmail.com>
2015-03-10 11:29:14 -0500

    teaching page

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,57 @@
 var easter_egg = new Konami(function() { $("#snippets-nav").removeClass('hide');});
+
+/**
+test1 = courseTermSorter('Spring 2015', 'Spring 2015') ==> 0
+test2 = courseTermSorter('Spring 2014', 'Spring 2015') ==> -1
+test3 = courseTermSorter('Spring 2015', 'Spring 2014') ==> 1
+test4 = courseTermSorter('Winter 2015', 'Spring 2015') ==> -1
+test5 = courseTermSorter('Spring 2015', 'Winter 2015') ==> 1
+*/
+function courseTermSorter(a, b) {
+	function pickTerm(x) {
+		switch(x) {
+		    case "Winter":
+				return 10;
+				break;
+		    case "Spring":
+				return 20;
+				break;
+			case "Summer 1":
+				return 30;
+				break;
+			case "Summer":
+				return 40;
+				break;
+			case "Summer 2":
+				return 50;
+				break;
+			case "Fall":
+				return 60;
+				break;
+		    default:
+				return 0;
+		}
+	};
+	var arrayA = a.split(' ');
+	var arrayB = b.split(' ');
+	if (arrayA.length == 2 && arrayB.length == 2) {
+		var yearA = arrayA[1];
+		var yearB = arrayB[1];
+		if (yearA > yearB) {
+			return 1;
+		} else if (yearA < yearB) {
+			return -1;
+		} else {
+			//Years are the same, compare seasons
+			var termA = pickTerm(arrayA[0]);
+			var termB = pickTerm(arrayB[0]);
+			if (termA > termB) {
+				return 1;
+			} else if (termA < termB) {
+				return -1;
+			}
+		}
+	}
+
+    return 0;
+}
